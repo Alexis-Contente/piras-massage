a4062bdd004df584fa74585e370ec705dd1e0fd1
Alexis-Contente <alexis.contente@gmail.com>
2023-05-25 15:50:36 +0200

    corrections done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ function About () {
             <p className="text__about">Cette application est un site qui permet de proposer les services de Piras Massage.<br />
                 Elle met en avant les prestations proposées par Davide Piras, un masseur thérapeute professionnel exerçant ce métier depuis plusieurs années, diplômé et certifié par plusieurs instituts.<br />
                 Vous retrouvez toutes les informations liées aux prestations dans l'onglet <Link className="link__about" href="/massages">MASSAGES</Link>.<br />
-                Pour plus d'informations ou pour prendre rendez-vous, n'hésitez pas à contacter Davide Piras via l"onglet <Link className="link__about" href="/massages">CONTACT</Link>.</p>
+                Pour plus d'informations ou pour prendre rendez-vous, n'hésitez pas à contacter Davide Piras via l'onglet <Link className="link__about" href="/massages">CONTACT</Link>.</p>
         </main>
     );
 }
