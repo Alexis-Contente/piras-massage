5a5292672c1905dd295a407112b76267f543c65d
Alexis-Contente <alexis.contente@gmail.com>
2023-06-26 15:56:58 +0200

    AddMap

--- src/app/degree/page.tsx
+++ src/app/degree/page.tsx
@@ -1,5 +1,6 @@
 import "../../../public/styles/degree.css";
 import IconFlower from "../../data/pictures/icons8-flower-50.png";
+import database from "../../db.json";
 
 import Image from "next/image";
 function Degree() {
@@ -8,115 +9,20 @@ function Degree() {
       <main className="container__degree">
         <h1 className="title__degree">Diplômes et qualifications</h1>
         <article className="degrees">
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Titre de Fascia Practicien
-              <br />
-              <span className="school">Ecole de Fascia Bien-Être, Cadenet</span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Certificat de Thaï Foot Reflexology
-              <br />
-              <span className="school">Institut A-Zen-Day, Paris</span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Diplôme de Thérapeute en Nuad Bo-Rarn Thaï Massage
-              <br />
-              <span className="school">
-                ITM (International Training Massage school), Thaïlande
-              </span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Diplômes Hot Stone Healing Massage et Aroma Herba Oil Massage
-              <br />
-              <span className="school">ITM-Spa Mantra, Thaïlande</span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Certificats de Reïki et d'Indian Head Massage
-              <br />
-              <span className="school">
-                Centre Musha Matombo à Harare, Zimbabwe
-              </span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Certificat de Shiatsu sur chaise "Amma Assi"
-              <br />
-              <span className="school">Shiatsu Thema à Versaille, France</span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Certificat de Shiatsu Esthétique "du visage"
-              <br />
-              <span className="school">Shiatsu Thema à Versaille, France</span>
-            </p>
-          </section>
-
-          <section className="degree">
-            <Image
-              className="icon__flower"
-              src={IconFlower}
-              alt="Icône d'une fleur"
-            ></Image>
-            <p className="detail__degree">
-              Certificat de Reïki 2 puis 3 niveau maitre
-              <br />
-              <span className="school">
-                Centre MundoReïki Hendaye Paris, France
-              </span>
-            </p>
-          </section>
+          {database.ecoles.map((ecole) => (
+            <section className="degree" key={ecole.id}>
+              <Image
+                className="icon__flower"
+                src={IconFlower}
+                alt="Icône d'une fleur"
+              ></Image>
+              <p className="detail__degree">
+                {ecole.degree}
+                <br />
+                <span className="school">{ecole.school}</span>
+              </p>
+            </section>
+          ))}
         </article>
       </main>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,112 @@
-import '../../public/styles/home.css';
-import ImgHome from '../data/pictures/massage2.jpg';
-import Image from 'next/image';
-import Link from 'next/link';
+import "../../public/styles/home.css";
+import ImgHome from "../data/pictures/massage2.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import database from "../db.json";
 
 function Home() {
-    return (
-        <>  
-            <main className="container__home">
-                <div className="container">
-                    <h1 className="title">Cabinet de massage et de soin énergétique</h1>
-                        <p className="intro">D'origine Sarde, et après 17 ans passés à Paris comme masseur thérapeute, j'ai pris la décision de revenir vivre en Sardaigne. Je propose des massages de bien-être, relaxants et revigorants pour l'ensemble des muscles du corps, mais aussi des massages thérapeutiques, (shiatsu, deep-tissue, reïki, fascia thérapie ...) dont chacun de nous a besoin sans toujours le savoir.<br />Ces massages ont des bienfaits physiologiques sur le corps mais également sur l’état psychologique, qui dépend souvent de la période que nous traversons, avec des effets durables qui permettent de rétablir un équilibre naturel du corps et de l'esprit.<br />Au cours de mon expérience, j'ai appris à envisager mon métier et mes interventions de façon globale et profonde dans la connaissance des imbrications multiples des mécanismes du corps humain et de ses relations et impacts sur le cerveau. Avant chaque séance, je prends le temps de discuter avec mon patient et de toucher quelques points précis de son corps afin de pouvoir cerner efficacement la méthode à utiliser. Cela a pour effet de répondre aux attentes et besoins de chaque patient de façon plus ciblée et personnelle.</p>
-                        <Image className="img__home" src={ImgHome} alt="Espace détente avec pierres et fleures"></Image>
-                        <article className="informations">
+  return (
+    <>
+      <main className="container__home">
+        <div className="container">
+          <h1 className="title">Cabinet de massage et de soin énergétique</h1>
+          <p className="intro">
+            D'origine Sarde, et après 17 ans passés à Paris comme masseur
+            thérapeute, j'ai pris la décision de revenir vivre en Sardaigne. Je
+            propose des massages de bien-être, relaxants et revigorants pour
+            l'ensemble des muscles du corps, mais aussi des massages
+            thérapeutiques (shiatsu, deep-tissue, reïki, fascia thérapie
+            ...) dont chacun de nous a besoin sans toujours le savoir.
+            <br />
+            Ces massages ont des bienfaits physiologiques sur le corps mais
+            également sur l’état psychologique, qui dépend souvent de la période
+            que nous traversons, avec des effets durables qui permettent de
+            rétablir un équilibre naturel du corps et de l'esprit.
+            <br />
+            Au cours de mon expérience, j'ai appris à envisager mon métier et
+            mes interventions de façon globale et profonde dans la connaissance
+            des imbrications multiples des mécanismes du corps humain et de ses
+            relations et impacts sur le cerveau. Avant chaque séance, je prends
+            le temps de discuter avec mon patient et de toucher quelques points
+            précis de son corps afin de pouvoir cerner efficacement la méthode à
+            utiliser. Cela a pour effet de répondre aux attentes et besoins de
+            chaque patient de façon plus ciblée et personnelle.
+          </p>
+          <Image
+            className="img__home"
+            src={ImgHome}
+            alt="Espace détente avec pierres et fleures"
+          ></Image>
+          <article className="informations">
+            <section className="cibles">
+              <h2 className="title__cibles">
+                A qui s'adressent mes soins et massages ?
+              </h2>
+              <p className="text__cibles">
+                Je propose mes services à une gamme variée de clients. Que ce
+                soit pour un enfant, une femme ou un homme, vous pouvez me
+                contacter pour des séances de massage et de soins énergétiques.
+                Ensemble, nous tenterons de trouver l’origine de votre mal et
+                les zones les plus touchées. Nous adopterons ensuite un
+                protocole de massages ciblés qui éliminera la source de votre
+                gêne de manière durable.
+              </p>
+            </section>
 
-                            <section className="cibles">
-                                <h2 className="title__cibles">A qui s'adressent mes soins et massages ?</h2>
-                                <p className="text__cibles">Je propose mes services à une gamme variée de clients. Que ce soit pour un enfant, une femme ou un homme, vous pouvez me contacter pour des séances de massage et de soins énergétiques. Ensemble, nous tenterons de trouver l’origine de votre mal et les zones les plus touchées. Nous adopterons ensuite un protocole de massages ciblés qui éliminera la source de votre gêne de manière durable.</p>
-                            </section>
-
-                            <section className="prestations">
-                                <h2 className="title__prestations">Une thérapie adaptée pour chaque besoin</h2>
-                                <div>
-                                    <p className="text__prestations">Chaque client est unique et a des besoins spécifiques. Pour un travail de qualité, j’adapte ma thérapie au besoin de chaque client. Pour ce faire, j’utilise diverses techniques que j’ai apprises durant mes formations :</p>
-                                    <div className="list__prestations">
-                                        <p>· Fascia thérapie.</p>
-                                        <p>· Massage Fascia.</p>
-                                        <p>· Massage & Thérapie Fascia.</p>
-                                        <p>· Massage Deep Tissue.</p>
-                                        <p>· Thaï massage.</p>
-                                        <p>· Aroma Herba Oil.</p>
-                                        <p>· Reiki.</p>
-                                </div>
-                                    <Link className="btn__prestations" href="/massages"><p className="link__prestations">Retrouvez tous les détails ici !</p></Link>
-                                </div>
-                            </section>
-
-                        </article>
+            <section className="prestations">
+              <h2 className="title__prestations">
+                Une thérapie adaptée pour chaque besoin
+              </h2>
+              <div>
+                <p className="text__prestations">
+                  Chaque client est unique et a des besoins spécifiques. Pour un
+                  travail de qualité, j’adapte ma thérapie au besoin de chaque
+                  client. Pour ce faire, j’utilise diverses techniques que j’ai
+                  apprises durant mes formations :
+                </p>
+                <div className="list__prestations">
+                  {database.massages.map((massage) => (
+                    <p key={massage.id}>· {massage.name}</p>
+                  ))}
                 </div>
-            </main>
-        </>
-    );
+                <Link className="btn__prestations" href="/massages">
+                  <p className="link__prestations">
+                    Retrouvez tous les détails ici !
+                  </p>
+                </Link>
+              </div>
+            </section>
+
+            <section className="localisation">
+              <h2 className="title__googlemap">Où j'exerce ?</h2>
+              <p className="text__googlemap">
+                J'exerce mon activité dans deux institus différents, tout deux
+                situé à Cagliari:
+                <br />
+                <a href="https://www.maison22cagliari.it/" className="institus">
+                  Maison 22 Esthetique
+                </a>
+                , le lundi ou mardi suivant les rendez-vous.
+                <br />
+                <a href="http://www.thalassos.it/" className="institus">
+                  Thalassos
+                </a>
+                , le vendredi après-midi et le samedi matin ou après-midi
+                suivant les rendez-vous.
+              </p>
+              <div className="googlemap">
+                <iframe
+                  className="map"
+                  src="https://storage.googleapis.com/maps-solutions-35cavkcvgj/locator-plus/t8no/locator-plus.html"
+                  loading="lazy"
+                ></iframe>
+              </div>
+            </section>
+          </article>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Home;
